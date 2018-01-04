7dc892795b987f718f6f3f030699db1710ace21d
Dmitry Bargatin <dibargatin@gmail.com>
2018-01-04 21:25:24 +0400

    Changed signature of bytes_to_base64() after discussion with Vitaly Lavrov.

--- src/main.rs
+++ src/main.rs
@@ -78,23 +78,23 @@ fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, ParseHexError> {
     Ok(bytes)
 }
 
-fn bytes_to_base64(bytes: Vec<u8>) -> String {
+fn bytes_to_base64(bytes: &[u8]) -> Vec<u8> {
     let mut trio: Vec<u8> = Vec::with_capacity(3);
-    let mut result: String = String::with_capacity(((bytes.len() as f32 / 3f32).ceil() * 4f32) as usize);
+    let mut result: Vec<u8> = Vec::with_capacity(((bytes.len() as f32 / 3f32).ceil() * 4f32) as usize);
 
     let alphabet = [        
-        'A','B','C','D','E','F','G','H',
-        'I','J','K','L','M','N','O','P',
-        'Q','R','S','T','U','V','W','X',
-        'Y','Z','a','b','c','d','e','f',
-        'g','h','i','j','k','l','m','n',
-        'o','p','q','r','s','t','u','v',
-        'w','x','y','z','0','1','2','3',
-        '4','5','6','7','8','9','+','/',
+        b'A',b'B',b'C',b'D',b'E',b'F',b'G',b'H',
+        b'I',b'J',b'K',b'L',b'M',b'N',b'O',b'P',
+        b'Q',b'R',b'S',b'T',b'U',b'V',b'W',b'X',
+        b'Y',b'Z',b'a',b'b',b'c',b'd',b'e',b'f',
+        b'g',b'h',b'i',b'j',b'k',b'l',b'm',b'n',
+        b'o',b'p',b'q',b'r',b's',b't',b'u',b'v',
+        b'w',b'x',b'y',b'z',b'0',b'1',b'2',b'3',
+        b'4',b'5',b'6',b'7',b'8',b'9',b'+',b'/',
     ];
     
     for b in bytes {
-        trio.push(b);
+        trio.push(*b);
 
         if trio.len() == 3 {
             result.push(alphabet[((trio[0] & 0b11111100) >> 2) as usize]);
@@ -109,14 +109,14 @@ fn bytes_to_base64(bytes: Vec<u8>) -> String {
     if trio.len() == 1 {
         result.push(alphabet[((trio[0] & 0b11111100) >> 2) as usize]);
         result.push(alphabet[((trio[0] & 0b00000011) << 4) as usize]);
-        result.push('=');
-        result.push('=');
+        result.push(b'=');
+        result.push(b'=');
     }
     else if trio.len() == 2 {
         result.push(alphabet[((trio[0] & 0b11111100) >> 2) as usize]);
         result.push(alphabet[(((trio[0] & 0b00000011) << 4) | ((trio[1] & 0b11110000) >> 4)) as usize]);
         result.push(alphabet[((trio[1] & 0b00001111) << 2) as usize]);
-        result.push('=');
+        result.push(b'=');
     }
 
     return result;
@@ -125,12 +125,14 @@ fn bytes_to_base64(bytes: Vec<u8>) -> String {
 fn main() {
     match hex_to_bytes("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") {
         Ok(bytes) =>  {
-            if bytes_to_base64(bytes) == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
-                println!("Done!");
+            let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+            if String::from_utf8(bytes_to_base64(&bytes)).expect("Not UTF-8") == expected {
+                println!("Task 1 from set 1: Done");
             } else {
-                println!("Fail!");
+                println!("Task 1 from set 1: Fail");
             }
         },
-        Err(e) => println!("Error: {}", e),
+        Err(e) => println!("Error 1.1: {}", e),
     }
 }
